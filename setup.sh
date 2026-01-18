65e1d03b4491c693ec2b7dc59a7700398a974161
verbraund <verbraund@gmail.com>
2026-01-18 20:20:25 +0200

    Fix setup shell script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ## Create link
-ln -sfn $PROJECT_ROOT/www/releases/1 $PROJECT_ROOT/www/current
+ln -sfn releases/1 $PROJECT_ROOT/www/current
 ## Create containers
 docker compose up -d
