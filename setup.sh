a2291f4fac0bd551338a13e5f5a08415b2cb9375
verbraund <verbraund@gmail.com>
2026-01-18 23:21:07 +0200

    Add docker install and gitlab-runner register

--- setup.sh
+++ setup.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+set -e
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+## INSTALL DOCKER
+if ! command -v docker >/dev/null 2>&1; then
+
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update -y
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo usermod -aG docker ${USER}
+    su - ${USER}
+fi
+
+docker --version
+
+## INSTALL GITLAB RUNNER
+if [ ! "$(docker ps -aq -f name=gitlab-runner)" ]; then
+
+    docker run -d --name gitlab-runner --restart always \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+      gitlab/gitlab-runner:latest
+
+    echo "service_healthcheck_timeout = 120" | sudo tee -a /srv/gitlab-runner/config/config.toml
+    docker restart gitlab-runner
+
+else
 
 ## Create link
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -sfn releases/1 $PROJECT_ROOT/www/current
+
 ## Create containers
 docker compose up -d
