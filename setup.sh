49762be52c1c3edb846ee64c0080429cb9935864
verbraund <verbraund@gmail.com>
2026-01-18 03:53:01 +0200

    Add sym link

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-## Create link
+#!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+## Create link
+ln -sfn $PROJECT_ROOT/www/releases/1 $PROJECT_ROOT/www/current
 ## Create containers
+docker compose up -d
